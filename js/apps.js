be636fcbf5b8129b665ecff047d7612f8783faea
Hassan Issa <hassan.issa2@outlook.com>
2022-04-18 23:19:54 -0400

    overlay employee card complete

--- js/apps.js
+++ js/apps.js
@@ -1,7 +1,6 @@
 // global variables
 let employees = [];
-const urlAPI = `https://randomuser.me/api/?results=12&inc=name, picture,
-email, location, phone, dob &noinfo &nat=US`
+const urlAPI = `https://randomuser.me/api/?results=12&inc=name, picture, email, location, phone, dob &noinfo &nat=US`
 const gridContainer = document.querySelector(".grid-container");
 const overlay = document.querySelector(".overlay");
 const modalContainer = document.querySelector(".modal-content");
@@ -36,13 +35,13 @@ function displayEmployees(employeeData) {
     </div>
     `
     });
+
     gridContainer.innerHTML = employeeHTML;
 }
 
 function displayModal(index) {
     // use object destructuring make our template literal cleaner
-    let { name, dob, phone, email, location: { city, street, state, postcode
-    }, picture } = employees[index];
+    let { name, dob, phone, email, location: { city, street, state, postcode }, picture } = employees[index];
     let date = new Date(dob.date);
     const modalHTML = `
     <img class="avatar" src="${picture.large}" />
@@ -62,18 +61,20 @@ function displayModal(index) {
 }
 
 gridContainer.addEventListener('click', e => {
+
     // make sure the click is not on the gridContainer itself
+    console.log("Hello")
     if (e.target !== gridContainer) {
-    // select the card element based on its proximity to actual element
-    clicked
+    // select the card element based on its proximity to actual element clicked
     const card = e.target.closest(".card");
     const index = card.getAttribute('data-index');
+    
     displayModal(index);
     }
 });
 
 modalClose.addEventListener('click', () => {
+    console.log("Hello")
     overlay.classList.add("hidden");
 });
-    
-    
+            
